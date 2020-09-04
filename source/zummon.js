8368da9b5f298793c4ce10cc3ea6b0cead06dcea
zummon <68579763+zummon@users.noreply.github.com>
2020-09-05 00:13:58 +0700

    update 2020-09-04

--- source/zummon.js
+++
@@ -1,6 +0,0 @@
-// var doc = document.querySelector('#doc')
-// var arr = []
-// doc.querySelectorAll('[id]').forEach(function(t){
-//     arr.push('#' + t.id)
-// })
-// console.log(arr.join(','));
