cea372a3b37c8ebae635f6b619032a234fc47608
zummontt <yoyotee235@gmail.com>
2020-12-06 22:22:01 +0700

    small adjust to speed

--- core_theme/dark.js
+++ core_theme/dark.js
@@ -2,13 +2,12 @@
   const elsModalDialog = document.querySelectorAll('.uk-modal-dialog')
 
   for (let z = 0; z < elsModalDialog.length; z++) {
-    elsModalDialog[z].classList.add('uk-background-secondary')
-    elsModalDialog[z].classList.add('uk-light')
+    elsModalDialog[z].classList.add('uk-background-secondary','uk-light')
   }
   
   document.querySelector('#above').classList.add('uk-light')
   document.querySelector('#nav').classList.add('uk-light')
   document.querySelector('#end').classList.add('uk-light')
-  document.documentElement.classList.add('uk-background-secondary')
+  document.documentElement.className = 'uk-background-secondary'
 
 })()
--- core_theme/light.js
+++ core_theme/light.js
@@ -2,13 +2,12 @@
   const elsModalDialog = document.querySelectorAll('.uk-modal-dialog')
 
   for (let z = 0; z < elsModalDialog.length; z++) {
-    elsModalDialog[z].classList.remove('uk-background-secondary')
-    elsModalDialog[z].classList.remove('uk-light')
+    elsModalDialog[z].classList.remove('uk-background-secondary','uk-light')
   }
 
   document.querySelector('#above').classList.remove('uk-light')
   document.querySelector('#nav').classList.remove('uk-light')
   document.querySelector('#end').classList.remove('uk-light')
-  document.documentElement.classList.remove('uk-background-secondary')
+  document.documentElement.className = ''
 
 })()
